c9f24075e40087f7b87354f01dbe3308b1a7ba6d
dmgig <dave.m.giglio@gmail.com>
2020-01-18 22:54:55 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,11 +14,11 @@ gulp build
 
 ls ./_posts/
 
-bundle exec jekyll build --verbose --config ${CONFIG_FILE}
+bundle exec jekyll build --config ${CONFIG_FILE}
 
 gulp copySiteToWebroot
 
 ssh useful@50.87.146.99 -p 2222 -o StrictHostKeyChecking=no "mkdir -p /home2/useful/${DOMAIN}/html/doc-search"
 
 rsync -acrv --stats -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
-   ./ useful@50.87.146.99:/home2/useful/${DOMAIN}/html/doc-search
+   ./html/doc-search/ useful@50.87.146.99:/home2/useful/${DOMAIN}/html/doc-search
