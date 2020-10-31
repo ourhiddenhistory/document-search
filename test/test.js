6114cc9bbf8ec2998198bd44083484ee8697cb6d
dmgig <dave.m.giglio@gmail.com>
2020-10-30 22:53:27 -0400

    Messing around with tests.

---
+++ test/test.js
@@ -0,0 +1,17 @@
+import Listing from '../src/js/classes/listing.js';
+
+console.log('Running tests...')
+
+describe("Color Code Converter", function() {
+  describe("RGB to Hex conversion", function() {
+    it("converts the basic colors", function() {
+
+    });
+  });
+
+  describe("Hex to RGB conversion", function() {
+    it("converts the basic colors", function() {
+
+    });
+  });
+});
